e89db3a389abf91a08576bd051847a4fc43ceaf6
sasa <sysmaaster@gmail.com>
2022-07-27 00:01:10 +0300

    06 - Components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-grid">
+      <Header />
+      <SideBar />
+      <MainContent />
+      <Footer />
+      
+      
     </div>
   );
 }
 
 export default App;
+
+const Header = () => {
+  return (
+    <header className="Header">
+      
+        <img className="logo-header" src='https://flyclipart.com/thumbs/raccoon-transparent-images-raccoon-logo-1172933.png' alt="logo" />
+      
+        
+      header тут?
+      
+    </header>
+  );
+}
+
+const SideBar = () => {
+  return (
+    <div className="SideBar">
+      
+      тут я SideBar  
+      
+    </div>
+  );
+}
+
+const MainContent = () => {
+  return (
+    <div className="MainContent">
+
+     я MainContent
+    
+    </div>
+  );
+}
+
+const Footer = () => {
+  return (
+    <footer className="Footer">
+      
+      я footer
+
+    </footer>
+  );
+}
