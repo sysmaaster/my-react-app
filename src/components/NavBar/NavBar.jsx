f7ffa5423a3ed2aa574a8b4f9ef2a24384334b0e
sasa <sysmaaster@gmail.com>
2022-07-31 19:17:02 +0300

    16- структура &&  (props - важнейшая тема)

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import s from './NavBar.module.css';
+
+const NavBar = () => {
+  return (
+    <div className={s.NavBar}>
+
+      <div className={s.item}><a href="#">Home</a></div>
+      <div className={`${s.item} ${s.active}`}><a href="#">Profile</a></div>
+      <div className={s.item}><a href="#">Contact</a></div>
+      <div className={s.item}><a href="#">Settings</a></div>
+      <div className={s.item}><a href="#">About As</a></div>
+      <div className={s.item}><a href="#">Entry</a></div>
+
+    </div>
+  );
+}
+export default NavBar
